faae537a3b30556d33412d52b96719bfd15520e0
hasib32 <hasib32@gmail.com>
2017-05-11 16:39:59 -0400

    Added command for downloading payment data

---
+++ app/Http/Controllers/PaymentDataController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PaymentDataController extends Controller
+{
+    public function getPaymentInfo()
+    {
+        dd('hello');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/payment', 'PaymentDataController@getPaymentInfo');
